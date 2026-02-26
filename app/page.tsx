dca65a8adf38bae50ee64d13e9408e0103d7bbc9
Alexandre Ferreira <breadmodz@gmail.com>
2026-02-26 04:31:20 -0300

    feat: authentication flow completed, animations added to display InputOTP and data validations.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@
 import 'dotenv/config'
 import Link from 'next/link'
 import { useState } from 'react'
-import { Mail, ChevronRight } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { Button } from '@/components/ui/button'
+import { motion, AnimatePresence } from 'framer-motion'
+import { Mail, ChevronRight, Check } from 'lucide-react'
 import {
   Select,
   SelectItem,
@@ -22,11 +25,48 @@ import {
 import { REGEXP_ONLY_DIGITS } from 'input-otp'
 
 import data from '@/hooks/data.json'
+import { authClient } from '@/lib/auth-client'
 
 export default function Home() {
+  const router = useRouter()
+
   const [otp, setOtp] = useState('')
+  const [codeHasSend, setCodeHasSend] = useState(false)
   const [selectedEmail, setSelectedEmail] = useState('')
   const [selectedFilial, setSelectedFilial] = useState<number>()
+
+  function resetFields() {
+    setOtp('')
+    setSelectedEmail('')
+    setCodeHasSend(false)
+  }
+
+  async function sendEmail() {
+    if (!selectedEmail) return
+
+    setCodeHasSend(true)
+
+    const response = await authClient.emailOtp.sendVerificationOtp({
+      email: selectedEmail,
+      type: 'sign-in',
+    })
+  }
+
+  async function verifyCode() {
+    if (!selectedEmail) return
+
+    const response = await authClient.signIn.emailOtp({
+      email: selectedEmail,
+      otp: otp,
+    })
+
+    if (response.data) {
+      resetFields()
+      router.push('/dashboard')
+    } else {
+      alert('Código inválido.')
+    }
+  }
 
   return (
     <div className="absolute inset-0 h-full w-full bg-[radial-gradient(rgba(229,231,235,0.10)_1px,transparent_1px)] bg-size-[14px_14px]">
@@ -35,6 +75,8 @@ export default function Home() {
           <div className="z-10 flex flex-col">
             <h1 className="mb-10 text-3xl font-bold">Autenticação</h1>
             <Select
+              value={selectedEmail}
+              disabled={codeHasSend}
               onValueChange={(value) => {
                 const filial = data.branches.find(
                   (item) => item.email === value
@@ -81,50 +123,100 @@ export default function Home() {
                 </SelectGroup>
               </SelectContent>
             </Select>
-            <button className="bg-muted border-border mt-6 mb-6 flex h-12 w-full cursor-pointer items-center justify-center gap-2 rounded-lg border transition-all hover:brightness-125">
-              <Mail size={24} className="text-foreground" />
-              <span className="text-foreground text-sm font-semibold">
-                Enviar código para o e-mail
-              </span>
-            </button>
-            <div className="flex flex-col gap-2">
-              <span className="text-muted-foreground text-sm">
-                Código enviado por e-mail
-              </span>
-              <InputOTP
-                maxLength={6}
-                id="digits-only"
-                pattern={REGEXP_ONLY_DIGITS}
-                value={otp}
-                onChange={(value) => setOtp(value)}
-              >
-                <InputOTPGroup className="*:data-[slot=input-otp-slot]:bg-muted *:data-[slot=input-otp-slot]:border-border w-full font-semibold *:data-[slot=input-otp-slot]:h-12 *:data-[slot=input-otp-slot]:flex-1 *:data-[slot=input-otp-slot]:text-xl">
-                  <InputOTPSlot index={0} />
-                  <InputOTPSlot index={1} />
-                  <InputOTPSlot index={2} />
-                </InputOTPGroup>
-                <InputOTPSeparator className="mx-2" />
-                <InputOTPGroup className="*:data-[slot=input-otp-slot]:bg-muted *:data-[slot=input-otp-slot]:border-border w-full font-semibold *:data-[slot=input-otp-slot]:h-12 *:data-[slot=input-otp-slot]:flex-1 *:data-[slot=input-otp-slot]:text-xl">
-                  <InputOTPSlot index={3} />
-                  <InputOTPSlot index={4} />
-                  <InputOTPSlot index={5} />
-                </InputOTPGroup>
-              </InputOTP>
-            </div>
-            <button className="group/button bg-foreground group text-background relative mt-6 inline-flex h-12 w-full flex-1 items-center justify-center gap-2 overflow-hidden rounded-lg px-6 py-3 text-base font-semibold whitespace-nowrap transition-all select-none hover:cursor-pointer lg:min-w-fit">
+            <Button
+              variant="outline"
+              onClick={sendEmail}
+              disabled={!selectedEmail || codeHasSend}
+              className="bg-muted border-border mt-6 mb-6 flex h-12 w-full cursor-pointer items-center justify-center gap-2 rounded-lg border transition-all hover:brightness-125"
+            >
+              <AnimatePresence mode="wait">
+                {!codeHasSend ? (
+                  <motion.div
+                    key="send"
+                    initial={{ x: 0, opacity: 1 }}
+                    exit={{ x: 60, opacity: 0 }}
+                    transition={{ duration: 0.3 }}
+                    className="flex items-center gap-2"
+                  >
+                    <Mail size={22} className="text-foreground" />
+                    <span className="text-foreground text-sm font-semibold">
+                      Enviar código para o e-mail
+                    </span>
+                  </motion.div>
+                ) : (
+                  <motion.div
+                    key="loading"
+                    initial={{ x: -60, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    exit={{ x: 60, opacity: 0 }}
+                    transition={{ duration: 0.3 }}
+                    className="flex items-center gap-2"
+                  >
+                    <Check size={22} className="text-foreground" />
+                    <span className="text-foreground text-sm font-semibold">
+                      Código enviado com sucesso
+                    </span>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </Button>
+            <AnimatePresence>
+              {codeHasSend && (
+                <motion.div
+                  initial={{ y: 40, opacity: 0, filter: 'blur(6px)' }}
+                  animate={{ y: 0, opacity: 1, filter: 'blur(0px)' }}
+                  exit={{ y: 40, opacity: 0, filter: 'blur(6px)' }}
+                  transition={{ duration: 0.4, ease: [0.4, 0, 0.2, 1] }}
+                  className="flex flex-col gap-2"
+                >
+                  <span className="text-muted-foreground text-sm">
+                    Código enviado por e-mail
+                  </span>
+
+                  <InputOTP
+                    value={otp}
+                    maxLength={6}
+                    id="digits-only"
+                    pattern={REGEXP_ONLY_DIGITS}
+                    onChange={(value) => setOtp(value)}
+                  >
+                    <InputOTPGroup className="*:data-[slot=input-otp-slot]:bg-muted *:data-[slot=input-otp-slot]:border-border w-full font-semibold *:data-[slot=input-otp-slot]:h-12 *:data-[slot=input-otp-slot]:flex-1 *:data-[slot=input-otp-slot]:text-xl">
+                      <InputOTPSlot index={0} />
+                      <InputOTPSlot index={1} />
+                      <InputOTPSlot index={2} />
+                    </InputOTPGroup>
+
+                    <InputOTPSeparator className="mx-2" />
+
+                    <InputOTPGroup className="*:data-[slot=input-otp-slot]:bg-muted *:data-[slot=input-otp-slot]:border-border w-full font-semibold *:data-[slot=input-otp-slot]:h-12 *:data-[slot=input-otp-slot]:flex-1 *:data-[slot=input-otp-slot]:text-xl">
+                      <InputOTPSlot index={3} />
+                      <InputOTPSlot index={4} />
+                      <InputOTPSlot index={5} />
+                    </InputOTPGroup>
+                  </InputOTP>
+                </motion.div>
+              )}
+            </AnimatePresence>
+            <Button
+              variant="outline"
+              onClick={verifyCode}
+              disabled={!codeHasSend}
+              className="group/button bg-foreground group text-background relative mt-6 inline-flex h-12 w-full flex-1 items-center justify-center gap-2 overflow-hidden rounded-lg px-6 py-3 text-base font-semibold whitespace-nowrap transition-all select-none hover:cursor-pointer lg:min-w-fit"
+            >
               <span className="mx-3.5 transition-all duration-400 group-hover:mx-0 group-hover:mr-6.5">
                 Entrar
               </span>
               <div className="absolute top-1/2 right-12 -translate-y-1/2 opacity-0 transition-all duration-300 ease-in-out group-hover:right-4 group-hover:opacity-100">
                 <ChevronRight size={24} className="text-black" />
               </div>
-            </button>
-            <Link
-              href="/dashboard"
-              className="text-muted-foreground mx-auto mt-3 w-fit text-sm hover:underline"
+            </Button>
+            <Button
+              variant="link"
+              onClick={resetFields}
+              className="text-muted-foreground mx-auto mt-3 w-fit cursor-pointer text-sm"
             >
               Reenviar código?
-            </Link>
+            </Button>
           </div>
         </div>
       </div>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,23 @@
+import { Resend } from 'resend'
 import { betterAuth } from 'better-auth'
-import { prismaAdapter } from 'better-auth/adapters/prisma'
-import prisma from '@/lib/prisma'
+import { emailOTP } from 'better-auth/plugins'
+
+const resend = new Resend(process.env.RESEND_API_KEY!)
 
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: 'postgresql',
-  }),
   emailAndPassword: {
     enabled: false,
   },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp }) {
+        resend.emails.send({
+          from: 'delivered@resend.dev',
+          to: email,
+          subject: 'Seu código de verificação',
+          html: `<h1>Código ${otp}</h1>`,
+        })
+      },
+    }),
+  ],
 })
