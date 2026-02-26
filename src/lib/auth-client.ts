ef74cded0bf7516b947d5693e519b75cac82ee00
Alexandre Ferreira <breadmodz@gmail.com>
2026-02-26 04:38:18 -0300

    fix: corrected problems with cors

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,5 +3,5 @@ import { emailOTPClient } from 'better-auth/client/plugins'
 
 export const authClient = createAuthClient({
   plugins: [emailOTPClient()],
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.BETTER_AUTH_URL,
 })
