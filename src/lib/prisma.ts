ca664b736d07394cf95c37d5bc7e539c6d3c38a8
Alexandre Ferreira <breadmodz@gmail.com>
2026-04-24 03:02:25 -0300

    chore: update Prisma dependencies and configuration
    - Bump @prisma/client and prisma versions to ^7.8.0
    - Add @prisma/extension-accelerate to package.json
    - Change database URL environment variable from DATABASE_URL to DIRECT_DATABASE_URL in prisma.config.ts
    - Remove seed.ts file as it is no longer needed
    - Update prisma.ts to use the withAccelerate extension
    - Update generated Zod manifest timestamp

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
@@ -13,7 +14,7 @@ const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     adapter,
-  })
+  }).$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
