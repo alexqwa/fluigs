9a9e77db379f4cee650913ef6f9c21a7383cd7c9
Alexandre Ferreira <breadmodz@gmail.com>
2026-04-24 03:34:08 -0300

    feat: remove Prisma extension for acceleration from client initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
@@ -14,7 +13,7 @@ const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     adapter,
-  }).$extends(withAccelerate())
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
